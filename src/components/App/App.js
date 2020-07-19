7b9b71125d94f2f1fda340643bfeba7dc647e810
Alex Gaytsuk <gayrsuk@gmail.com>
2020-07-19 23:24:21 +0300

    project structure

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-
+import './App.css';
+import Header from "../Header";
+import Navbar from "../Navbar";
+import MainContent from "../MainContent";
+import RandomContent from "../RandomContentBlock";
+import Footer from "../Footer";
 
 const App = () => {
-  return (
-    <div className="App">
-      <h1>IncubatorBlog</h1>
-    </div>
-  );
-}
+    return (
+        <div className="App">
+            <Header/>
+            <Navbar/>
+            <MainContent/>
+            <RandomContent/>
+            <Footer/>
+        </div>
+    );
+};
 
 export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import n from './Navbar.module.scss';
+
+const Navbar = () => {
+    return (
+        <section className={n.navBar}>
+            Navbar
+        </section>
+    )
+};
+
+export default Navbar;
