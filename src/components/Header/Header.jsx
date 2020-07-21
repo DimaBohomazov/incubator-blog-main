9737070963aae1b0006378e1fdbdc19218a83d48
Bohomazov Dmitriy <bohomazov.dmitriy@gmail.com>
2020-07-22 00:57:10 +0300

    Revert "Merge pull request #5 from DimaBohomazov/header-marckup"
    This reverts commit 3de2508ba2df1a35699d788571fda1e81578fac0, reversing
    changes made to 094664e730fce9d234540be9684cbab355782bf4.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,90 +1,24 @@
 import React from "react";
 
 import h from './Header.module.scss';
-import '../../assets/stylesheets/variables.css'
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {
-    faHome,
-    faBell,
-    faSearch,
-    faCaretDown,
-} from "@fortawesome/free-solid-svg-icons";
-import {NavLink} from "react-router-dom";
+import './../../cssVariables/variables.css'
 
 const Header = () => {
 
     const {
-        pItem,
         header,
-        pItemSpan,
         logoBlock,
-        headerMenu,
-        searchBlock,
-        headerAccount,
-        headerImgBlock,
-        headerMenuItem,
-        headerMenuBlock,
-        headerContainer,
-        headerNotification,
-        headerAccountCaret,
-        countOfNotifications,
-        headerLogoSearchBlock,
     } = h;
 
     return (
         <header className={header}>
-            <div className={headerContainer}>
-
-                <div className={headerLogoSearchBlock}>
-                    <NavLink to='/' className={logoBlock}>
-                        IncubatorBlog
-                    </NavLink>
-
-                    <div className={searchBlock}>
-                        <label htmlFor="search">
-                            <FontAwesomeIcon icon={faSearch}/>
-                        </label>
-                        <input type='search' id='search' placeholder='Search'/>
-                    </div>
-                </div>
-
-
-                <div className={headerMenuBlock}>
-
-                    <ul className={headerMenu}>
-                        <li className={headerMenuItem}>
-                            <NavLink to="/">
-                                <p className={pItem}>
-                                    <FontAwesomeIcon icon={faHome}/>
-                                </p>
-                                <p className={pItemSpan}>
-                                    <span>Home</span>
-                                </p>
-                            </NavLink>
-                        </li>
-                        <li className={`${headerMenuItem} ${headerNotification}`}>
-                            <p className={countOfNotifications}>19</p>
-                            <NavLink to="/">
-                                <p className={pItem}>
-                                    <FontAwesomeIcon icon={faBell}/>
-                                </p>
-                                <p className={pItemSpan}>
-                                    <span>Notifications</span>
-                                </p>
-                            </NavLink>
-                        </li>
-                    </ul>
-
-                    <NavLink to='/' className={headerAccount}>
-                        <p className={headerImgBlock}></p>
-                        <div className={headerAccountCaret}>
-                            <span>Me</span>
-                            <FontAwesomeIcon icon={faCaretDown}/>
-                        </div>
-                    </NavLink>
-
-                </div>
+            <div className={logoBlock}>
+                IncubatorBlog
             </div>
+            Search
+            Home
+            Notifications
+            Account
         </header>
     )
 };
