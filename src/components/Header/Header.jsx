89a0d6be8f06a06ca3e266cf186e22217161f339
Aleksandr Gaytsuk <A.Gaytsuk@s1.ventalab.ua>
2020-07-21 17:37:13 +0300

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -24,46 +24,65 @@ const Header = () => {
         headerImgBlock,
         headerMenuItem,
         headerMenuBlock,
+        headerContainer,
+        headerNotification,
+        headerAccountCaret,
+        countOfNotifications,
+        headerLogoSearchBlock,
     } = h;
 
     return (
         <header className={header}>
-            <div className={logoBlock}>
-                IncubatorBlog
-            </div>
-            <div className={searchBlock}>
-                <label htmlFor="search">
-                    <FontAwesomeIcon icon={faSearch}/>
-                </label>
-                <input type='search' id='search' placeholder='Search'/>
-            </div>
-            <div className={headerMenuBlock}>
-                <ul className={headerMenu}>
-                    <li className={headerMenuItem}>
-                        <a href="/">
-                            <p className={pItem}>
-                                <FontAwesomeIcon icon={faHome}/>
-                            </p>
-                            <p className={pItemSpan}>
-                                <span>Home</span>
-                            </p>
-                        </a>
-                    </li>
-                    <li className={headerMenuItem}>
-                        <a href="/">
-                            <p className={pItem}>
-                                <FontAwesomeIcon icon={faBell}/>
-                            </p>
-                            <p className={pItemSpan}>
-                                <span>Notifications</span>
-                            </p>
-                        </a>
-                    </li>
-                </ul>
-                <div className={headerAccount}>
-                    <p className={headerImgBlock}></p>
-                    <span>Me</span>
-                    <FontAwesomeIcon icon={faCaretDown} />
+            <div className={headerContainer}>
+
+                <div className={headerLogoSearchBlock}>
+                    <div className={logoBlock}>
+                        IncubatorBlog
+                    </div>
+
+                    <div className={searchBlock}>
+                        <label htmlFor="search">
+                            <FontAwesomeIcon icon={faSearch}/>
+                        </label>
+                        <input type='search' id='search' placeholder='Search'/>
+                    </div>
+                </div>
+
+
+                <div className={headerMenuBlock}>
+
+                    <ul className={headerMenu}>
+                        <li className={headerMenuItem}>
+                            <a href="/">
+                                <p className={pItem}>
+                                    <FontAwesomeIcon icon={faHome}/>
+                                </p>
+                                <p className={pItemSpan}>
+                                    <span>Home</span>
+                                </p>
+                            </a>
+                        </li>
+                        <li className={`${headerMenuItem} ${headerNotification}`}>
+                            <p className={countOfNotifications}>19</p>
+                            <a href="/">
+                                <p className={pItem}>
+                                    <FontAwesomeIcon icon={faBell}/>
+                                </p>
+                                <p className={pItemSpan}>
+                                    <span>Notifications</span>
+                                </p>
+                            </a>
+                        </li>
+                    </ul>
+
+                    <div className={headerAccount}>
+                        <p className={headerImgBlock}></p>
+                        <div className={headerAccountCaret}>
+                            <span>Me</span>
+                            <FontAwesomeIcon icon={faCaretDown}/>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </header>
