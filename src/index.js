7978ef3fd776a3fcffa967e1fb653671cae63c99
Alex Gaytsuk <gayrsuk@gmail.com>
2020-07-21 21:20:46 +0300

    refactored file strucrure at project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from "react-router-dom";
-import './index.css'
+import './assets/stylesheets/index.css'
 import Routes from './routes'
 import Header from "./components/Header";
 import Footer from "./components/Footer";
