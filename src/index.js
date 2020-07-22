5bc7568ab515d6f792faaa27f88db7abf3a57199
Bohomazov Dmitriy <bohomazov.dmitriy@gmail.com>
2020-07-22 23:21:01 +0300

    check bitbucked

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import {createStore, compose, applyMiddleware} from 'redux'
 import {Provider} from 'react-redux'
 import thunk from 'redux-thunk'
 
-
 const composeEnhancers =
   typeof window === 'object' &&
   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
